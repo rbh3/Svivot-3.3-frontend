dd8976124f8cb49618e84b2357aa489d6c43310a
rbh3 <34860619+rbh3@users.noreply.github.com>
2018-06-11 21:42:23 +0300

    tokens

--- routing/components/Services/serviceController.js
+++ routing/components/Services/serviceController.js
@@ -1,40 +1,51 @@
 angular.module('citiesApp')
-    // .service('myService', function () { this.set = function() {return "hello"} })
     .service('setHeadersToken',[ '$http', function ($http) {
-
         let token = ""
-
         this.set = function (t) {
             token = t
             $http.defaults.headers.common[ 'x-access-token' ] = t
             // $httpProvider.defaults.headers.post[ 'x-access-token' ] = token
             console.log("set")
-
+            
         }
+        }])
 
-        this.userName='shir'
- 
-
-    }])
-
-    
-    .controller('serviceController', ['$location', '$http', 'setHeadersToken','localStorageModel', function ($location, $http, setHeadersToken,localStorageModel) {
+    .service('checkToken',['setHeadersToken','localStorageModel', function (setHeadersToken,localStorageModel) {
+        if(localStorageModel.getLocalStorage('token')!=null)
+             setHeadersToken.set(localStorageModel.getLocalStorage('token'))
+        console.log("HYYYYYY")
+        }])
 
 
+    .controller('serviceController', ['$location', '$http', 'setHeadersToken','localStorageModel','checkToken' ,function ($location, $http, setHeadersToken,localStorageModel,checkToken) {
         self = this;
 
         self.directToPOI = function () {
             $location.path('/poi')
         }
 
-        let serverUrl = 'http://localhost:8080/'
+        let serverUrl = 'http://localhost:3000/'
 
         let user = {
-            userName: "Shir",
-            password: "abcd",
-            isAdmin: true
+            Username: "test1",
+            Password: "11111111",
+            Fname: "rr",
+            Lname:"ttt",
+            City: "gfgf",
+            Country: "ggfgf",
+            Email:"fggbv",
+            categories: 1,
+            categories: 2,
+            Q1:"fvfvfv",
+            A1:"fvfvfv" ,
+            Q2: "fVfv",
+            A2: "fdcdc" 
         }
 
+        let user2 = {
+            userName: "test1",
+            password: "11111111",
+        }
 
         self.signUp = function () {
             // register user
@@ -50,13 +61,16 @@ angular.module('citiesApp')
 
         self.login = function () {
             // register user
-            $http.post(serverUrl + "Users/login", user)
+            $http.post(serverUrl + "Users/authenticate", user2)
                 .then(function (response) {
+                    if(response.data.message==="bad values"){
+                        alert("can't login")
+                        return
+                    }
                     //First function handles success
                     self.login.content = response.data.token;
                     setHeadersToken.set(self.login.content)
-
-
+                    localStorageModel.addLocalStorage('token', self.login.content)
                 }, function (response) {
                     //Second function handles error
                     self.login.content = "Something went wrong";
@@ -65,11 +79,10 @@ angular.module('citiesApp')
 
         self.reg = function () {
             // register user
-            $http.post(serverUrl + "reg/", user)
+            $http.get(serverUrl + "POI/reg/FavoritesByUsername/2")
                 .then(function (response) {
                     //First function handles success
                     self.reg.content = response.data;
-
                 }, function (response) {
                     self.reg.content = response.data
                     //Second function handles error
@@ -77,9 +90,7 @@ angular.module('citiesApp')
                 });
         }
 
-        self.addTokenToLocalStorage = function () {
-            localStorageModel.addLocalStorage('token', self.login.content)
-        }
+      
 
 
 
