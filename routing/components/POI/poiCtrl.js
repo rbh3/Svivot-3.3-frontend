898e5422725a43edd28b88887f9b8ea48785566b
Dor Bartal <dorr.bartal@gmail.com>
2018-06-17 11:29:16 +0300

    pois

--- routing/components/POI/poiCtrl.js
+++ routing/components/POI/poiCtrl.js
@@ -1,27 +1,18 @@
 angular.module('citiesApp')
- .controller('poiCtrl', [function() {
- 
+    .controller('poiCtrl', ['$location', '$http', function ($location, $http) {
+        let self = this;
+        let serverUrl = 'http://localhost:3000/'
 
-    self = this;
+        self.getPOIbyID = function () {
+            $http.get(serverUrl + "POI/getPOIbyID/33")
+                .then(function (response) {
+                    //First function handles success
+                    self.POI = response.data;
 
-    self.cities = {
-        1 : {name:"Paris", state: "France", image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/f5/7c/f2/eiffel-tower-priority.jpg"}
-        ,2 : {name:"Jerusalem", state: "Israel", image: "https://cdni.rt.com/files/2017.12/article/5a3fe04efc7e93cd698b4567.jpg"}
-        ,3 : {name:"London", state: "England", image: "http://www.ukguide.co.il/Photos/England/London/British-Royal-Tour.jpg"}
-    }
-
-    self.selectedCity= function (id){
-
-        console.log (self.selected )
-    }
-
-    self.addToCart = function (id, city) {
-
-        console.log(id)
-        console.log(city)
-        console.log(self.amount[id])
-
-
-    }
-
- }]);
+                }, function (response) {
+                    self.random = response.data
+                    //Second function handles error
+                    // self.reg.content = "Something went wrong";
+                });
+        }
+    }]);
