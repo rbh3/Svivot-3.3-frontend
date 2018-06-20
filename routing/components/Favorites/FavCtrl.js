63871711813b4688afa27d2d87c8394f9e01c204
rbh3 <34860619+rbh3@users.noreply.github.com>
2018-06-20 11:46:17 +0300

    my fav

--- routing/components/Favorites/FavCtrl.js
+++ routing/components/Favorites/FavCtrl.js
@@ -1,9 +1,9 @@
 angular.module('citiesApp')
-    .controller('FavCtrl', ['$routeParams', '$http','checkToken','$location','$rootScope', function ($routeParams, $http,checkToken,$location,$rootScope) {
+    .controller('FavCtrl', ['$routeParams', '$http', 'checkToken', '$location', '$rootScope', function ($routeParams, $http, checkToken, $location, $rootScope) {
         let self = this;
         let serverUrl = 'http://localhost:3000/'
-        self.PicSelected="/assets/img/unlike.JPG"
-        self.chooseFilter="No-Filter"
+        self.PicSelected = "/assets/img/unlike.JPG"
+        self.chooseFilter = "No-Filter"
 
         self.cats = [
             { id: 1, text: 'Shopping' },
@@ -15,78 +15,47 @@ angular.module('citiesApp')
         //self.sortValue = '';
         //self.sortByRank = function () {self.sortValue = '-Rank'; }
 
-        self.init=function(){
+        self.init = function () {
             checkToken.check();
         }
 
-        self.getFav=function(){
-            if(self.POI&& $rootScope.localFav){
-            self.myFav=[];
-            for(let i=0;i<self.POI.length;i++){
-                var ind=$rootScope.localFav.indexOf(self.POI[i].ID);
-                if(ind>-1)
-                    self.myFav.push(self.POI[i]);
+        self.goTo = function (name) {
+            $location.path('/poi/id/' + name)
+        }
+
+        self.checkifFav = function (name) {
+            if ($rootScope.localFav.length > 0) {
+                if ($rootScope.localFav.filter(value => value.ID == name.ID).length > 0)
+                    return true;
+                else
+                    return false;
             }
-            return self.myFav;
-        }
-        }
-      
-        self.getAllPoi = function (id) {
-            $http.get(serverUrl + "POI/getAllPOI/")
-                .then(function (response) {
-                    //First function handles success
-                    self.POI = response.data;
-                }, function (response) {
-                    self.POI = response.data;
-                    //Second function handles error
-                    // self.reg.content = "Something went wrong";
-                });
         }
 
-        self.goTo=function(name){
-            $location.path('/poi/id/'+name)
+        self.getCatName = function (name) {
+            return self.cats[name - 1].text
         }
 
-        self.checkifFav=function(name){
-            if($rootScope.localFav.includes(name))
-                return true;
-            else
-                return false;
-        }
-
-        self.getCatName=function(name){
-            return self.cats[name-1].text
-        }
-
-        self.getCatID=function(mycat){
-            if(self.cats.filter(cat => cat.text === mycat)[0]) 
+        self.getCatID = function (mycat) {
+            if (self.cats.filter(cat => cat.text === mycat)[0])
                 return self.cats.filter(cat => cat.text === mycat)[0].id
             else
                 return undefined
         }
 
-        self.saveFav=function(name){
-           if($rootScope.localFav.includes(name))
-           {
-                let i=$rootScope.localFav.indexOf(name);
-                if(i>-1)
-                {
-                    for(var j=0;j<self.myFav.length;j++)
-                        if(name===self.myFav[j].ID)
-                            self.myFav.splice(j,1);
-                    $rootScope.localFav.splice(i,1);
-                }
-           }
-           else{
-                $rootScope.localFav.push(name); 
-                for(var j=0;j<self.POI.length;j++)
-                    if(name===self.POI[j].ID)
-                        self.myFav.push(self.POI[j])
-           }
+        self.saveFav = function (name) {
+            if ($rootScope.localFav.filter(value => value.ID == name.ID).length > 0) {
+                let i = $rootScope.localFav.findIndex(x => x.ID === name.ID)
+                if (i > -1)
+                    $rootScope.localFav.splice(i, 1);
+            }
+            else {
+                $rootScope.localFav.push(name);
+            }
         }
 
-        self.loc=function(id){
-            return $rootScope.localFav.indexOf(id);
-
-         }
+        self.loc = function (id) {
+            let i = $rootScope.localFav.findIndex(x => x.ID === id)
+            return i;
+        }
     }]);
